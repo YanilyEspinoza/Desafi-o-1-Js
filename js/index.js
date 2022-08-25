082125053b72fdeb56242e2e26a7d32da0f5b4d4
Yanily Espinoza <yanilyespinozaarg@gmail.com>
2022-08-25 14:21:21 -0300

    Desafío 1 de js, repeticion de palabras y división

---
+++ js/index.js
@@ -0,0 +1,20 @@
+//Repeticiones de un texto
+
+let repeticiones = parseInt(prompt("Ingrese número de repeticiones deseadas"));
+let texto = prompt ("Ingresar texto a repetir");
+
+debugger
+
+for (let i=0; i<repeticiones; i++){
+    console.log(texto);
+}
+
+//División de números
+
+/*let numero1 = parseInt(prompt("Ingrese número a dividir"))
+let numero2 = parseInt(prompt("Ingrese divisor"))
+
+debugger
+
+let resultado = numero1 / numero2;
+alert(resultado);*/
